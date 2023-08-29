10fcb9e4e234cb6364882733e35030842988f95a
Karan Cheema <karancheema@protonmail.com>
2023-08-29 21:56:21 +0530

    Add files via upload

---
+++ Introduction To JavaScript/Lesson 3/lesson03_sample1.js
@@ -0,0 +1,8 @@
+function getGreeting(firstName, lastName) {
+    var greeting = "Hello, " + firstName + " " + lastName + "!";
+    return greeting;
+}
+
+var message = getGreeting("John", "Doe");
+
+alert(message)
