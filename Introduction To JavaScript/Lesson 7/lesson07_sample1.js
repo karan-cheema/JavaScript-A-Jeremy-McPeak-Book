31ca32ecfe731785e03101e5a274d87110aa8508
Karan Cheema <karancheema@protonmail.com>
2023-09-06 22:49:17 +0530

    Add files via upload

---
+++ Introduction To JavaScript/Lesson 7/lesson07_sample1.js
@@ -0,0 +1,22 @@
+function getMonthDate(index) {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    return months[index];
+
+}
+
+function abbrName(text) {
+    return text.substr(0, 3);
+}
+
+var date = new Date();
+var messageParts = [
+    "Hello, ",
+    prompt("Please enter your name", "Please enter your name"),
+    ". Today is ",
+    abbrName (getMonthName(date.getMonth())),
+    " ",
+    date.getFullYear()
+];
+
+alert(messageParts.join(""));
